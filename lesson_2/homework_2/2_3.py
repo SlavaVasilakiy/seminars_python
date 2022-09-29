9c6bf419f28470735792dbd386a33654b226004d
vasilakiy <slk55slk@gmail.com>
2022-09-29 20:06:44 +0300

    lesson_2 , task_3

--- lesson_2/homework_2/2_3.py
+++ lesson_2/homework_2/2_3.py
@@ -1,2 +1,9 @@
 # 3. Задайте список из n чисел, заполненный по формуле (1 + 1/n) ** n и выведите на экран их сумму.
 # Для n = 6: [2, 2, 2, 2, 2, 3] -> 13
+
+n = int(input('input a number: '))
+a = [round((1 + 1 / x) ** x) for x in range(1, n + 1)]
+sum = 0
+for i in a:
+    sum = sum + i
+print(f'{a} sum = {sum}')
