63eaef6d57d1a2828171d7b31a7a1071097e1193
vasilakiy <slk55slk@gmail.com>
2022-09-29 21:11:51 +0300

    lesson_2 , task_4

--- lesson_2/homework_2/2_4.py
+++ lesson_2/homework_2/2_4.py
@@ -3,3 +3,11 @@
 # Position two: 3
 # Number of elements: 5
 # -> [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+num = int(input('input number of elemts: '))
+a = []
+for i in range(-num, num + 1):
+    a.append(i)
+print(a)
+pos_1 = int(input('input position one: '))
+pos_2 = int(input('input position two: '))
+print(f'product of numbers on positions ({pos_1}) and ({pos_2}) = {a[pos_1 - 1] * a[pos_2 - 1]}')
