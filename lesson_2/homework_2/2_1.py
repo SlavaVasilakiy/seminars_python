9f56af0d9d28d1fcd6d51ca2df3acdf78cee9a1b
vasilakiy <slk55slk@gmail.com>
2022-09-29 18:19:21 +0300

    lesson_2 , task_2

--- lesson_2/homework_2/2_1.py
+++ lesson_2/homework_2/2_1.py
@@ -3,3 +3,14 @@
 # - 6782 -> 23
 # - 0.67 -> 13
 # - 198.45 -> 27
+
+n = float(input('Input a number: '))
+n_length = len(str(n)) - 1
+m = int(n * 10 ** n_length)
+print(m)
+suma = 0
+while m > 0:
+    digit = m % 10
+    suma = suma + digit
+    m = m // 10
+print(suma)
