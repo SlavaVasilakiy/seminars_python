3ec12f47058b2df4ceed231f649de57aa70812bb
vasilakiy <slk55slk@gmail.com>
2022-10-06 20:06:04 +0300

    lesson_3 , hm_3_2

--- lesson_3/homework_3/hm_3_2.py
+++ lesson_3/homework_3/hm_3_2.py
@@ -13,3 +13,24 @@
 # out
 # [2, 2, 4, 8, 8]
 # [16, 16, 4]
+
+from random import randint
+
+n = int(input('Input list range: '))
+my_list = [randint(1, 10) for i in range(n)]
+print(my_list)
+
+
+def prod_pairs(my_list: list):
+    res_list = []
+    len_list = len(my_list)
+
+    for k in range(len_list // 2):
+        res_list.append(my_list[k] * my_list[len_list - k - 1])
+
+    if len_list % 2:
+        res_list.append(my_list[len_list // 2])
+    return res_list
+
+
+print(prod_pairs(my_list))
